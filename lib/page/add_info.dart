ae5d3661d35d0d193d28d4ccf49363267d81d53f
NguyenHuuThao251 <nguyenhuuthao2001@gmail.com>
2023-02-28 08:02:03 +0700

    commit chart

--- lib/page/add_info.dart
+++ lib/page/add_info.dart
@@ -1,6 +1,8 @@
+import 'dart:ffi';
+
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AddInfor extends StatefulWidget {
   @override
@@ -12,8 +14,24 @@ class _AddInforState extends State<AddInfor> {
   final nameMoney = TextEditingController();
   final numberMoney = TextEditingController();
   final contentMoney = TextEditingController();
-
+  DateTime date = DateTime.now();
   late DatabaseReference dbRef;
+  int selectedItem = 0;
+  final List<String> _item = [
+    "Food",
+    "Transfer",
+    "Transportation",
+    "Education"
+  ];
+
+  final List<String> _itemImage = [
+    "assets/icon/icon_add_food.svg",
+    "assets/icon/icon_add_transfer.svg",
+    "assets/icon/icon_add_transportation.svg",
+    "assets/icon/icon_add_transportation.svg"
+  ];
+
+  final List<int> _itemInt = [0, 1, 2, 3];
 
   @override
   void initState() {
@@ -32,74 +50,175 @@ class _AddInforState extends State<AddInfor> {
           ],
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                'Inserting data in Firebase Realtime Database',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: nameMoney,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Payment name',
-                    hintText: 'Enter payment name'),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: numberMoney,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Amount spent',
-                    hintText: 'Enter amount spent'),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: contentMoney,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Payment content',
-                    hintText: 'Enter payment content'),
-              ),
-              MaterialButton(
-                onPressed: () {
-                  Map<String, String> money = {
-                    'namepayment': nameMoney.text,
-                    'numbermoney': numberMoney.text,
-                    'contentmoney': contentMoney.text
-                  };
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    'Inserting data in Firebase Realtime Database',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black38,
+                      ),
+                    ),
+                    child: DropdownButton<int>(
+                        value: selectedItem,
+                        items: _itemInt
+                            .map((e) => DropdownMenuItem(
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      SvgPicture.asset(
+                                        _itemImage[e],
+                                        height: 40,
+                                        width: 40,
+                                      ),
+                                      const SizedBox(
+                                        width: 30,
+                                      ),
+                                      Text(_item[e])
+                                    ],
+                                  ),
+                                ),
+                                value: e))
+                            .toList(),
+                        hint: Text('Payment Type'),
+                        dropdownColor: Colors.white,
+                        isExpanded: true,
+                        underline: Container(),
+                        onChanged: ((value) {
+                          setState(() {
+                            selectedItem = value!;
+                          });
+                        }),
+                        selectedItemBuilder: (BuildContext context) => _itemInt
+                            .map(
+                              (e) => Row(
+                                children: [
+                                  SvgPicture.asset(
+                                    _itemImage[e],
+                                    height: 20,
+                                    width: 20,
+                                  ),
+                                  const SizedBox(
+                                    width: 30,
+                                  ),
+                                  Text(_item[e]),
+                                ],
+                              ),
+                            )
+                            .toList()),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    controller: nameMoney,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Payment name',
+                        hintText: 'Enter payment name'),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    controller: numberMoney,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Amount spent',
+                        hintText: 'Enter amount spent'),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    controller: contentMoney,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Payment content',
+                        hintText: 'Enter payment content'),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                      width: double.infinity,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black38,
+                        ),
+                      ),
+                      child: TextButton(
+                        onPressed: () async {
+                          DateTime? newDate = await showDatePicker(
+                              context: context,
+                              initialDate: date,
+                              firstDate: DateTime(2023),
+                              lastDate: DateTime(2100));
+                          if(newDate == null) return;
+                          setState(() {
+                            date = newDate;
+                          });
+                        },
+                        child: Text('Date: ${date.year}/${date.month}/${date.day}'),
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      Map<String, String> money = {
+                        'typepayment': _item[selectedItem],
+                        'namepayment': nameMoney.text,
+                        'numbermoney': numberMoney.text,
+                        'contentmoney': contentMoney.text,
+                        'date': date.toString()
+                      };
 
-                  dbRef.push().set(money);
-                  Navigator.pop(context);
-                },
-                child: const Text('Insert Data'),
-                color: Colors.blue,
-                textColor: Colors.white,
-                height: 40,
+                      dbRef.push().set(money);
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Insert Data'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    height: 40,
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/page/chart.dart
+++ lib/page/chart.dart
@@ -3,15 +3,112 @@ import 'package:flutter/material.dart';
 class Chart extends StatefulWidget {
   @override
   State<Chart> createState() => _ChartState();
-
 }
 
 class _ChartState extends State<Chart> {
+  List day = ['Day', 'Week', 'Month', 'Year'];
+  int indexCurrent = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Chart"),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Statistics',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ...List.generate(4, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              indexCurrent = index;
+
+                            });
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: indexCurrent != index
+                                  ? Color.fromARGB(255, 143, 141, 141)
+                                  : Color.fromARGB(255, 47, 125, 121),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(
+                              day[index],
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        );
+                      }),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 40,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              'Expense',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_downward_sharp,
+                              color: Colors.grey,
+                            )
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(8)
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
